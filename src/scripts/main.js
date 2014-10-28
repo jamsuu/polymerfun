b79459ac4b7a778fdf055d870824774fd64a6096
David Vu <davidv@worldmanager.com>
2014-10-28 15:33:53 +1100

    Initial Commit

---
+++ src/scripts/main.js
@@ -0,0 +1,31 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  classes = require('bespoke-classes'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
+
+var fx = require('bespoke-fx');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  keys(),
+  touch(),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+  forms(),
+  fx()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
